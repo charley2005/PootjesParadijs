2245e916ec099644f96ccbbd06cc0d3739ffe100
Dylanjagt <123470056+Dylanjagt@users.noreply.github.com>
2024-01-26 12:27:56 +0100

    Add files via upload
    patatje

---
+++ PootjesParadijs-main/PootjesParadijs/pages/homepage.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Dierenopvang & co</title>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+
+
+
+<nav class="navbar">
+  <div><img src="../assets/logo.png" class="logo"></div>
+  <div>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+</div>
+</nav>
+
+<div class="container py-4">
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Pootjesparadijs</h1>
+        <p class="col-md-8 fs-4">Welkom op onze site van het Pootjesparadijs, de plek waar uw huisdier in het zonnetje word gezet. Wilt u meer weten over wie wij zijn of wat wij doen kijk dan gerust veder, mocht u interessen hebben kunt u altijd contact opnemen. Wilt u meer weten over contact informatie, of wilt u een afspraak maken klik dan op de knop hieronder. </p>
+        <img class="happy-dog" src="https://www.24petwatch.com/content/dam/24-petwatch/images/other/dog-lifespan_preview.jpg" width="300px" alt="">
+        <button class="btn btn-primary btn-lg" type="button">klik hier</button>
+      </div>
+    </div>
+    <hr class="border border-primary border-2 opacity-50">
+
+    <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 text-bg-dark rounded-3">
+          <h2>Change the background</h2>
+          <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+          <button class="btn btn-outline-light" type="button">Example button</button>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+          <h2>Add borders</h2>
+          <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+          <button class="btn btn-outline-secondary" type="button">Example button</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+
+    <a href="" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+
+  </footer>
+</div>
+
+
+
+
+
+
+<script>
+    var triggerEl = document.querySelector("#navId a");
+    bootstrap.Tab.getInstance(triggerEl).show(); // Select tab by name
+</script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
